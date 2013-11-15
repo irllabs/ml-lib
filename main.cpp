ce76222d942e50a91e16448f89243ce7b4762e49
Jamie Bullock <jamie@jamiebullock.com>
2013-11-15 20:38:32 +0000

    Add setter implementations

--- main.cpp
+++ main.cpp
@@ -12,15 +12,36 @@
 
 #include <flext.h>
 
+#include <vector>
+#include <string>
+
+
+static const std::string weight_delimiter = ":";
+
 class svm : public flext_base
 {
     FLEXT_HEADER_S(svm,flext_base,setup);
     
 public:
     svm(int argc,t_atom *argv)
-    : model(NULL), x_space(NULL)
+    : model(NULL), x_space(NULL), weight_labels(1000, 0), weight_values(1000, 0)
     {
         post("creating svm object");
+        param.svm_type = C_SVC;
+        param.kernel_type = RBF;
+        param.degree = 3;
+        param.gamma = 0;
+        param.coef0 = 0;
+        param.nu = 0.5;
+        param.cache_size = 100;
+        param.C = 1;
+        param.eps = 1e-3;
+        param.p = 0.1;
+        param.shrinking = 1;
+        param.probability = 0;
+        param.nr_weight = 0;
+        param.weight_label = &weight_labels[0];
+        param.weight = &weight_values[0];
     }
     
     ~svm()
@@ -50,7 +71,7 @@ protected:
         FLEXT_CADDATTR_SET(c, "cachesize", set_cachesize);
         FLEXT_CADDATTR_SET(c, "shrinking", set_shrinking);
         FLEXT_CADDATTR_SET(c, "estimates", set_estimates);
-        FLEXT_CADDATTR_SET(c, "weight", set_weight);
+        FLEXT_CADDATTR_SET(c, "weight", set_weights);
         FLEXT_CADDATTR_SET(c, "mode", set_mode);
         
         FLEXT_CADDATTR_GET(c, "type", get_type);
@@ -64,7 +85,7 @@ protected:
         FLEXT_CADDATTR_GET(c, "cachesize", get_cachesize);
         FLEXT_CADDATTR_GET(c, "shrinking", get_shrinking);
         FLEXT_CADDATTR_GET(c, "estimates", get_estimates);
-        FLEXT_CADDATTR_GET(c, "weight", get_weight);
+        FLEXT_CADDATTR_GET(c, "weights", get_weights);
         FLEXT_CADDATTR_GET(c, "mode", get_mode);
         
 //        FLEXT_CADDMETHOD(c, 0, add);
@@ -98,7 +119,7 @@ protected:
     void set_cachesize(int cachesize);
     void set_shrinking(int shrinking);
     void set_estimates(int estimates);
-    void set_weight(int weight);
+    void set_weights(const AtomList &weights);
     void set_mode(int mode); // cross validation mode
     
     // Attribute Getters
@@ -113,7 +134,7 @@ protected:
     void get_cachesize(int &cachesize) const;
     void get_shrinking(int &shrinking) const;
     void get_estimates(int &estimates) const;
-    void get_weight(int &weight) const;
+    void get_weights(AtomList &lst) const;
     void get_mode(int &mode) const;
     
 private:
@@ -137,7 +158,7 @@ private:
     FLEXT_CALLVAR_I(get_cachesize, set_cachesize);
     FLEXT_CALLVAR_I(get_shrinking, set_shrinking);
     FLEXT_CALLVAR_I(get_estimates, set_estimates);
-    FLEXT_CALLVAR_I(get_weight, set_weight);
+    FLEXT_CALLVAR_V(get_weights, set_weights);
     FLEXT_CALLVAR_I(get_mode, set_mode);
     
     // Instance variables
@@ -145,15 +166,189 @@ private:
     svm_parameter param;
     svm_problem prob;
     svm_node *x_space;
+    
     int nr_fold;
+    
+    std::vector<int> weight_labels;
+    std::vector<double> weight_values;
+    
 };
 
-void svm::set_type(int type)
+void svm::set_type(int type)    
 {
+    switch (type)
+    {
+        case C_SVC:
+        case NU_SVC:
+        case ONE_CLASS:
+        case EPSILON_SVR:
+        case NU_SVR:
+            param.svm_type = type;
+            break;
+            
+        default:
+            post("Error: invalid SVM type, send a 'bang' to the first inlet for available types");
+            break;
+    }
+}
+
+void svm::set_kernel(int kernel)    
+{
+    switch (kernel)
+    {
+        case LINEAR:
+        case POLY:
+        case RBF:
+        case SIGMOID:
+        case PRECOMPUTED:
+            param.kernel_type = kernel;
+            break;
+            
+        default:
+            post("Error: invalid kernel type, send a 'bang' to the first inlet for available types");
+            break;
+    }
+}
+
+void svm::set_degree(int degree)    
+{
+    param.degree = degree;
+}
+
+void svm::set_gamma(float gamma)    
+{
+    param.gamma = (double)gamma;
+}
+
+void svm::set_coef0(float coef0)    
+{
+    param.coef0 = (double)coef0;
+}
+
+void svm::set_cost(float cost)    
+{
+    param.C = (double)cost;
+}
+
+void svm::set_nu(float nu)    
+{
+    param.nu = (double)nu;
+}
+
+void svm::set_epsilon(float epsilon)    
+{
+    param.eps = (double)epsilon;
+}
+
+void svm::set_cachesize(int cachesize)    
+{
+    param.cache_size = cachesize;
+}
+
+void svm::set_shrinking(int shrinking)    
+{
+    switch (shrinking)
+    {
+        case 0:
+        case 1:
+            param.shrinking = shrinking;
+            break;
+            
+        default:
+            error("Error: shrinking must either be 0 (off) or 1 (on)");
+            break;
+    }
+}
+
+void svm::set_estimates(int estimates)    
+{
+    switch (estimates)
+    {
+        case 0:
+        case 1:
+            param.probability = estimates;
+            break;
+            
+        default:
+            error("Error: probability estimates must either be 0 (off) or 1 (on)");
+            break;
+    }
+}
+
+void svm::set_weights(const AtomList &weights)
+{
+    param.nr_weight = weights.Count();
+    weight_labels.clear();
+    weight_values.clear();
+    
+    for (uint16_t count = 0; count < param.nr_weight; ++count)
+    {
+        std::string weight = GetString(GetSymbol(weights[count]));
+        std::string::size_type location = weight.find(weight_delimiter);
+        
+        if (location == std::string::npos)
+        {
+            error("Error: no ':' found, weights must be a list of class:weight pairs");
+            return;
+        }
+        int label = atoi(weight.substr(0, location).c_str());
+        double value = atof(weight.substr(location, weight.length()).c_str());
+        
+        weight_labels[count] = label;
+        weight_values[count] = value;
+    }
+}
+
+void svm::set_mode(int mode)
+{
+    if (mode < 2)
+    {
+        error("Error: n-fold cross validation: n must >= 2");
+        return;
+    }
+    nr_fold = mode;
 }
 
 void svm::get_type(int &type) const
 {
+    
 }
+void svm::get_kernel(int &type) const
+{    
+}
+void svm::get_degree(int &type) const    
+{    
+}
+void svm::get_gamma(float &gamma) const    
+{    
+}
+void svm::get_coef0(float &coef0) const    
+{    
+}
+void svm::get_cost(float &cost) const    
+{    
+}
+void svm::get_nu(float &nu) const    
+{    
+}
+void svm::get_epsilon(float &epsilon) const    
+{    
+}
+void svm::get_cachesize(int &cachesize) const    
+{    
+}
+void svm::get_shrinking(int &shrinking) const    
+{    
+}
+void svm::get_estimates(int &estimates) const    
+{    
+}
+void svm::get_weights(AtomList &lst) const
+{    
+}
+void svm::get_mode(int &mode) const    
+{    
+}
+
 
 FLEXT_NEW_V("svm", svm)
