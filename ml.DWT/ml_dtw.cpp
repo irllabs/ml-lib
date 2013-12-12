f206709197ace3909eb841d92840b84e4e8c8449
Jamie Bullock <jamie@jamiebullock.com>
2013-12-12 19:20:11 +0000

    Extend DTW over all observations and give classification decision to nearest vector

--- ml.DWT/ml_dtw.cpp
+++ ml.DWT/ml_dtw.cpp
@@ -19,20 +19,42 @@
 #include "ml.h"
 #include "UCR_DTW.h"
 
+#include <algorithm>
+#include <numeric>
+
 #include <flext.h>
 #include <math.h>
 
 namespace ml
 {
- 
-typedef struct query_
+    
+template<typename T>
+struct sum_square
 {
-    double *data;
-    uint32_t num_items;
-    double sum;
-    double sum_squared;
+    T operator()(const T& Left, const T& Right) const
+    {
+        return (Left + Right*Right);
+    }
+};
+    
+typedef struct result_
+{
+    double label;
+    long long location;
+    double distance;
+    
+    bool operator < (const result_& result) const
+    {
+        return (distance < result.distance);
+    }
 }
-query;
+result;
+
+bool compare_observation_label(observation ob1, observation ob2)
+{
+    return (ob1.label < ob2.label);
+}
+    
 
 // TODO: refactor this and normalizer into a standard interface that can be used by the normalize method
 class z_normalizer
@@ -97,7 +119,7 @@ private:
     // Attribute wrappers
     FLEXT_CALLVAR_F(get_window_size, set_window_size);
     
-    void dtw_(const query &query, const observation &observation, long long &location, double &distance);
+    void dtw_(std::vector<double> query, const observation &observation, long long &location, double &distance);
 
     
     // Instance variables
@@ -122,33 +144,33 @@ void ml_dtw::get_window_size(float &size) const
     size = window_size;
 }
     
-void ml_dtw::dtw_(const query &query, const observation &observation, long long &location, double &distance)
+void ml_dtw::dtw_(std::vector<double> query, const observation &observation, long long &location, double &distance)
 {
     
-    if (query.num_items == 0 || observation.features.size() == 0)
+    if (query.size() == 0 || observation.features.size() == 0)
     {
         return;
     }
     
-    /// For every EPOCH points, all cummulative values, such as ex (sum), ex2 (sum square), will be restarted for reducing the floating point error.
     uint32_t EPOCH = 100000;
     
     z_normalizer normalizer;
-    double sum = 0.0;
-    double sum_squared = 0.0;
+    double sum = std::accumulate(query.begin(), query.end(), 0.0);
+    double sum_squared = std::accumulate(query.begin(), query.end(), 0.0, sum_square<double>());
     long long location_ = 0;
-    uint32_t query_length = query.num_items;
+    uint32_t query_length = query.size();
     uint32_t window_size_absolute = floor(window_size * query_length);
     int kim = 0;
     int keogh = 0;
     int keogh2 = 0;
     double dist = 0;
-    double lower_bound_kim = 0;
-    double lower_bound_keogh = 0;
-    double lower_bound_keogh2 = 0;
+    double lower_bound_kim = 0.0;
+    double lower_bound_keogh = 0.0;
+    double lower_bound_keogh2 = 0.0;
     double mean = 0.0;
     double std_deviation = 0.0;
     double best_so_far = INF;
+    double *query_data = &query[0];
     double *lower_envelope = new double[query_length];
     double *upper_envelope = new double[query_length];
     double *query_ordered = new double[query_length];
@@ -165,58 +187,52 @@ void ml_dtw::dtw_(const query &query, const observation &observation, long long 
     int *order = new int[query_length];
     Index *Q_tmp = new Index[query_length];
     
-    /// Do z-normalize the query, keep in same array, q
-    normalizer.mean = query.sum / query_length;
-    normalizer.std_deviation = sqrt((query.sum_squared / query_length) - mean * mean);
+    normalizer.mean = sum / query_length;
+    normalizer.std_deviation = sqrt((sum_squared / query_length) - mean * mean);
     
     for(uint32_t i = 0 ; i < query_length ; ++i)
     {
-        query.data[i] = normalizer.normalize(query.data[i]);
+        query_data[i] = normalizer.normalize(query_data[i]);
     }
     
-    /// Create envelop of the query: lower envelop, l, and upper envelop, u
-    lower_upper_lemire(query.data, query_length, window_size_absolute, lower_envelope, upper_envelope);
+    lower_upper_lemire(query_data, query_length, window_size_absolute, lower_envelope, upper_envelope);
     
-    /// Sort the query one time by abs(z-norm(q[i]))
     for(uint32_t i = 0; i < query_length; ++i)
     {
-        Q_tmp[i].value = query.data[i];
+        Q_tmp[i].value = query_data[i];
         Q_tmp[i].index = i;
     }
     qsort(Q_tmp, query_length, sizeof(Index), comp);
     
-    /// also create another arrays for keeping sorted envelop
     for(uint32_t i = 0; i < query_length; ++i)
     {
         int o = Q_tmp[i].index;
         order[i] = o;
-        query_ordered[i] = query.data[o];
+        query_ordered[i] = query_data[o];
         upper_ordered[i] = upper_envelope[o];
         lower_ordered[i] = lower_envelope[o];
     }
     
-    /// Initial the cummulative lower bound
     for(uint32_t i = 0; i < query_length; ++i)
     {   cumulative_bound[i]=0;
         cumulative_bound1[i]=0;
         cumulative_bound2[i]=0;
     }
     
-    uint32_t i = 0;          /// current index of the data in current chunk of size EPOCH
-    uint32_t j = 0;          /// the starting index of the data in the circular array, t
+    uint32_t i = 0;
+    uint32_t j = 0;
     sum = sum_squared = 0;
     bool done = false;
     int it = 0;
     uint32_t epoch = 0;
     int32_t k = 0;
-    long long I;    /// the starting index of the data in current chunk of size EPOCH
+    long long I;
     double current = 0.0;
 
     feature_map::const_iterator feature_iterator = observation.features.begin();
     
     while(!done)
     {
-        /// Read first m-1 points
         epoch = 0;
         if (it==0)
         {
@@ -236,7 +252,6 @@ void ml_dtw::dtw_(const query &query, const observation &observation, long long 
             buffer[k] = buffer[EPOCH-query_length+1+k];
         }
         
-        /// Read buffer of size EPOCH or when all data has been read.
         epoch = query_length - 1;
         while(epoch < EPOCH)
         {
@@ -250,8 +265,6 @@ void ml_dtw::dtw_(const query &query, const observation &observation, long long 
             feature_iterator++;
         }
         
-        /// Data are read in chunk of size EPOCH.
-        /// When there is nothing to read, the loop is end.
         if (epoch <= query_length-1)
         {
             done = true;
@@ -260,66 +273,50 @@ void ml_dtw::dtw_(const query &query, const observation &observation, long long 
         {
             lower_upper_lemire(buffer, epoch, window_size_absolute, lower_buff, upper_buffer);
             
-            /// Just for printing a dot for approximate a million point. Not much accurate.
             if (it%(1000000/(EPOCH-query_length+1))==0)
             {
-                post(".");
+//                post(".");
             }
             
-            /// Do main task here..
-            sum=0; sum_squared=0;
+            sum=0;
+            sum_squared=0;
+            
             for(i = 0; i < epoch; i++)
             {
-                /// A bunch of data has been read and pick one of them at a time to use
                 current = buffer[i];
                 
-                /// Calcualte sum and sum square
                 sum += current;
                 sum_squared += current*current;
                 
-                /// t is a circular array for keeping current data
                 time_series[i%query_length] = current;
-                
-                /// Double the size for avoiding using modulo "%" operator
                 time_series[(i%query_length)+query_length] = current;
                 
-                /// Start the task when there are more than m-1 points in the current chunk
                 if( i >= query_length-1 )
                 {
                     mean = sum/query_length;
                     std_deviation = sum_squared/query_length;
                     std_deviation = sqrt(std_deviation-mean*mean);
                     
-                    /// compute the start location of the data in the current circular array, t
                     j = (i+1)%query_length;
-                    /// the start location of the data in the current chunk
                     I = i-(query_length-1);
                     
-                    /// Use a constant lower bound to prune the obvious subsequence
-                    lower_bound_kim = lb_kim_hierarchy(time_series, query.data, j, query_length, mean, std_deviation, best_so_far);
+                    lower_bound_kim = lb_kim_hierarchy(time_series, query_data, j, query_length, mean, std_deviation, best_so_far);
                     
                     if (lower_bound_kim < best_so_far)
                     {
-                        /// Use a linear time lower bound to prune; z_normalization of t will be computed on the fly.
-                        /// uo, lo are envelop of the query.
                         lower_bound_keogh = lb_keogh_cumulative(order, time_series, upper_ordered, lower_ordered, cumulative_bound1, j, query_length, mean, std_deviation, best_so_far);
+                        
                         if (lower_bound_keogh < best_so_far)
                         {
-                            /// Take another linear time to compute z_normalization of t.
-                            /// Note that for better optimization, this can merge to the previous function.
                             for(k = 0; k < 0 || (uint32_t)k < query_length; k++)
                             {
                                 z_normalized_time_series[k] = (time_series[(k+j)] - mean)/std_deviation;
                             }
                             
-                            /// Use another lb_keogh to prune
-                            /// qo is the sorted query. tz is unsorted z_normalized data.
-                            /// l_buff, u_buff are big envelop for all data in this chunk
                             lower_bound_keogh2 = lb_keogh_data_cumulative(order, z_normalized_time_series, query_ordered, cumulative_bound2, lower_buff+I, upper_buffer+I, query_length, mean, std_deviation, best_so_far);
+                            
                             if (lower_bound_keogh2 < best_so_far)
                             {
-                                /// Choose better lower bound between lb_keogh and lb_keogh2 to be used in early abandoning DTW
-                                /// Note that cb and cb2 will be cumulative summed here.
                                 if (lower_bound_keogh > lower_bound_keogh2)
                                 {
                                     cumulative_bound[query_length-1]=cumulative_bound1[query_length-1];
@@ -333,23 +330,29 @@ void ml_dtw::dtw_(const query &query, const observation &observation, long long 
                                         cumulative_bound[k] = cumulative_bound[k+1]+cumulative_bound2[k];
                                 }
                                 
-                                /// Compute DTW and early abandoning if possible
-                                dist = dtw(z_normalized_time_series, query.data, cumulative_bound, query_length, window_size_absolute, best_so_far);
+                                dist = dtw(z_normalized_time_series, query_data, cumulative_bound, query_length, window_size_absolute, best_so_far);
                                 
                                 if( dist < best_so_far )
-                                {   /// Update bsf
-                                    /// loc is the real starting location of the nearest neighbor in the file
+                                {
                                     best_so_far = dist;
                                     location_ = (it)*(EPOCH-query_length+1) + i-query_length+1;
                                 }
-                            } else
+                            }
+                            else
+                            {
                                 keogh2++;
-                        } else
+                            }
+                        }
+                        else
+                        {
                             keogh++;
-                    } else
+                        }
+                    }
+                    else
+                    {
                         kim++;
+                    }
                     
-                    /// Reduce obsolute points from sum and sum square
                     sum -= time_series[j];
                     sum_squared -= time_series[j]*time_series[j];
                 }
@@ -357,9 +360,13 @@ void ml_dtw::dtw_(const query &query, const observation &observation, long long 
             
             /// If the size of last chunk is less then EPOCH, then no more data and terminate.
             if (epoch<EPOCH)
+            {
                 done=true;
+            }
             else
+            {
                 it++;
+            }
         }
     }
     
@@ -374,12 +381,13 @@ void ml_dtw::dtw_(const query &query, const observation &observation, long long 
     delete[] cumulative_bound;
     delete[] cumulative_bound1;
     delete[] cumulative_bound2;
+    delete[] z_normalized_time_series;
     delete[] time_series;
-    delete[] order;
-    delete[] Q_tmp;
     delete[] buffer;
     delete[] upper_buffer;
     delete[] lower_buff;
+    delete[] order;
+    delete[] Q_tmp;
     
 }
 
@@ -392,41 +400,62 @@ void ml_dtw::classify(int argc, const t_atom *argv)
         return;
     }
     
-    query query;
+    std::vector<double> query;
     
-    query.num_items = argc;
-    query.data = new double[query.num_items];
-    query.sum = 0.0;
-    query.sum_squared = 0.0;
-    
-    long long location = 0;
-    double distance = 0;
-    
-    for (uint32_t index = 0; index < query.num_items; ++index)
+    for (uint32_t index = 0; index < (uint32_t)argc; ++index)
     {
         double value = GetAFloat(argv[index]);
-        query.data[index] = value;
-        query.sum += value;
-        query.sum_squared += value * value;
+        query.push_back(value);
     }
     
-    dtw_(query, observations[0], location, distance);
+    std::vector<observation>::iterator observation_iterator;
+    std::vector<result> results;
+    std::sort(observations.begin(), observations.end(), compare_observation_label);
     
-    delete [] query.data;
+    AtomList triples;
     
-    AtomList result;
+    for (observation_iterator = observations.begin(); observation_iterator != observations.end(); ++observation_iterator)
+    {
+        long long location = 0;
+        double distance = 0;
+        result result;
+        t_atom label_a;
+        t_atom location_a;
+        t_atom distance_a;
+        
+        dtw_(query, *observation_iterator, location, distance);
+        
+        result.location = location;
+        result.distance = distance;
+        result.label = observation_iterator->label;
+
+        SetDouble(&distance_a, result.distance);
+        SetInt(label_a, result.label);
+        SetInt(location_a, result.location);
+        
+        triples.Append(label_a);
+        triples.Append(location_a);
+        triples.Append(distance_a);
+        
+        results.push_back(result);
+    }
     
+    std::sort(results.begin(), results.end());
+    result winner = results[0];
+    
+    AtomList result_;
+    
+    t_atom label_a;
     t_atom location_a;
-    t_atom distance_a;
     
-    // Need to call SetDouble() first or label_a gets corrupted. Bug in Flext?
-    SetDouble(&distance_a, distance);
-    SetInt(location_a, location);
+    SetInt(label_a, winner.label);
+    SetInt(location_a, winner.location);
     
-    result.Append(location_a);
-    result.Append(distance_a);
+    result_.Append(label_a);
+    result_.Append(location_a);
     
-    ToOutList(0, result);
+    ToOutList(0, result_);
+    ToOutList(1, triples);
 }
     
 
