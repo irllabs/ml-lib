171393d9edf6c5438e78ad6a6823ef7ff4456385
Jamie Bullock <jamie@jamiebullock.com>
2013-12-24 17:16:13 +0000

    Initial MLP implementation based on GRT

--- ml.mlp/ml_mlp.cpp
+++ ml.mlp/ml_mlp.cpp
@@ -17,7 +17,7 @@
  */
 
 #include "ml.h"
-#include "fann.h"
+#include "GRT.h"
 
 #include <flext.h>
 
@@ -37,7 +37,24 @@ namespace ml
     public:
         ml_mlp()
         {
-            post("ml.mlp: Multilayer Perceptron based on the FANN library");
+            post("ml.mlp: Multilayer Perceptron based on the GRT library");
+            
+            trainingData.setInputAndTargetDimensions(2, 1);
+            
+            GRT::UINT numInputNeurons = trainingData.getNumInputDimensions();
+            GRT::UINT numHiddenNeurons = 2;
+            GRT::UINT numOutputNeurons = trainingData.getNumTargetDimensions();
+            
+            mlp.init(numInputNeurons, numHiddenNeurons, numOutputNeurons);
+            
+            mlp.setMaxNumEpochs( 100 );
+            mlp.setMinChange( 1.0e-2 );
+            mlp.setNumRandomTrainingIterations( 5 );
+            mlp.setUseValidationSet( true );
+            mlp.setValidationSetSize( 20 );
+            mlp.setRandomiseTrainingOrder( true );
+            mlp.enableScaling( true );
+            
         }
         
         ~ml_mlp()
@@ -52,8 +69,18 @@ namespace ml
         }
         
         // Methods
+        virtual void add(int argc, const t_atom *argv);
+        virtual void save(const t_symbol *path) const;
+        virtual void load(const t_symbol *path);
+        virtual void normalize();
+        virtual void train();
+        virtual void clear();
+        virtual void classify(int argc, const t_atom *argv);
+        virtual void usage();
         
         // Attribute Setters
+        
+        // TODO: set / get the stuff currently in the constructor
         
         // Attribute Getters
         
@@ -61,16 +88,217 @@ namespace ml
         // Attribute wrappers
         
         // Instance variables
+        GRT::MLP mlp;
+        GRT::LabelledRegressionData trainingData;
     };
     
     // Utility functions
-    
+    std::string get_symbol_as_string(const t_symbol *symbol)
+    {
+        const char *c_string = flext::GetAString(symbol);
+        std::string cpp_string;
+        
+        if (c_string == NULL)
+        {
+            error("symbol was NULL");
+        }
+        else
+        {
+            cpp_string.assign(c_string);
+        }
+        
+        return cpp_string;
+    }
     
     // Attribute setters
     
     // Attribute getters
     
     // Methods
+    void ml_mlp::add(int argc, const t_atom *argv)
+    {
+        
+        GRT::UINT numInputNeurons = mlp.getNumInputNeurons();
+        GRT::UINT numOutputNeurons = mlp.getNumOutputNeurons();
+        GRT::UINT combinedVectorSize = numInputNeurons + numOutputNeurons;
+        
+        // TODO: also check if trainingData inputDimensions / outpuDimensions matches?
+        
+        if (argc < 0 || (unsigned)argc != combinedVectorSize)
+        {
+            error("invalid input length, expected %d, got %d", combinedVectorSize, argc);
+            return;
+        }
+        
+        if (numInputNeurons != trainingData.getNumInputDimensions() || numOutputNeurons != trainingData.getNumTargetDimensions())
+        {
+            error("inconsistent MLP dimensions. To set, use attributes: num_input_neurons, num_output_neurons");
+            return;
+        }
+        
+        GRT::VectorDouble inputVector(numInputNeurons);
+        GRT::VectorDouble targetVector(numOutputNeurons);
+        
+        for (uint32_t index = 0; index < (unsigned)argc; ++index)
+        {
+            float value = GetAFloat(argv[index]);
+
+            if (index < numOutputNeurons)
+            {
+                targetVector[index] = value;
+            }
+            else
+            {
+                inputVector[index - numOutputNeurons] = value;
+            }
+        
+        }
+
+        trainingData.addSample(inputVector, targetVector);
+    }
+
+    void ml_mlp::save(const t_symbol *path) const
+    {
+        if (trainingData.getNumSamples() == 0)
+        {
+            error("no observations added, use 'add' to add training data");
+            return;
+        }
+        
+        std::string file_path = get_symbol_as_string(path);
+        
+        if (file_path.empty())
+        {
+            error("path string is empty");
+            return;
+        }
+        
+        bool success = trainingData.saveDatasetToFile(file_path);
+        
+        if (!success)
+        {
+            error("unable to save training data to path: %s", file_path.c_str());
+        }
+    }
+    
+    void ml_mlp::load(const t_symbol *path)
+    {
+        std::string file_path = get_symbol_as_string(path);
+        
+        if (file_path.empty())
+        {
+            error("path string is empty");
+            return;
+        }
+
+        bool success = trainingData.loadDatasetFromFile(file_path);
+        
+        if (!success)
+        {
+            error("unable to load training data from path: %s", file_path.c_str());
+        }
+    }
+    
+    void ml_mlp::normalize()
+    {
+        error("function not implemented");
+    }
+    
+    void ml_mlp::train()
+    {
+        if (trainingData.getNumSamples() == 0)
+        {
+            error("no observations added, use 'add' to add training data");
+            return;
+        }
+        
+        bool success = mlp.train(trainingData);
+        
+        if (!success)
+        {
+            error("training failed");
+            return;
+        }
+        
+        t_atom a_num_classes;
+        
+        SetInt(a_num_classes, mlp.getNumClasses());
+        ToOutAnything(1, s_train, 1, &a_num_classes);
+    }
+    
+    void ml_mlp::clear()
+    {
+        t_atom status;
+        SetBool(status, true);
+        
+        trainingData.clear();
+        
+        ToOutAnything(1, s_cleared, 1, &status);
+    }
+    
+    void ml_mlp::classify(int argc, const t_atom *argv)
+    {
+        if (trainingData.getNumSamples() == 0)
+        {
+            error("no observations added, use 'add' to add training data");
+            return;
+        }
+        
+        if (mlp.getTrained() == false)
+        {
+            error("model has not been trained, use 'train' to train the model");
+            return;
+        }
+        
+        GRT::UINT numInputNeurons = mlp.getNumInputNeurons();
+        GRT::VectorDouble query(numInputNeurons);
+        
+        if (argc < 0 || (unsigned)argc != numInputNeurons)
+        {
+            error("invalid input length, expected %d, got %d", numInputNeurons, argc);
+        }
+
+        for (uint32_t index = 0; index < (uint32_t)argc; ++index)
+        {
+            double value = GetAFloat(argv[index]);
+            query[index] = value;
+        }
+        
+        bool success = mlp.predict(query);
+        
+        if (success == false)
+        {
+            error("unable to classify input");
+            return;
+        }
+        
+        GRT::VectorDouble regressionData = mlp.getRegressionData();
+        GRT::VectorDouble::size_type numOutputDimensions = regressionData.size();
+        
+        if (numOutputDimensions != mlp.getNumOutputNeurons())
+        {
+            error("invalid output dimensions: %d", numOutputDimensions);
+            return;
+        }
+        
+        AtomList result;
+        
+        for (uint32_t index = 0; index < numOutputDimensions; ++index)
+        {
+            t_atom value_a;
+            SetDouble(&value_a, regressionData[index]);
+            result.Append(value_a);
+        }
+        
+        ToOutList(0, result);
+        
+    }
+    
+    void ml_mlp::usage()
+    {
+        error("function not implemented");
+    }
+
     
     
     FLEXT_LIB("ml.mlp", ml_mlp);
