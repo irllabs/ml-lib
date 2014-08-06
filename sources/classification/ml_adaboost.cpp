38a10670071f0557cb946c079a2b5fdefc1f310a
Jamie Bullock <jamie@jamiebullock.com>
2014-08-06 12:31:25 +0100

    Add 'add_weak_classifier' to ml.adaboost

--- sources/classification/ml_adaboost.cpp
+++ sources/classification/ml_adaboost.cpp
@@ -50,7 +50,10 @@ namespace ml
             post_stream << "prediction_method:\tinteger (" << GRT::AdaBoost::MAX_VALUE << ":MAX_VALUE or " <<  GRT::AdaBoost::MAX_POSITIVE_VALUE << ":MAX_POSITIVE_VALUE) sets the Adaboost prediction method (default " << GRT::AdaBoost::MAX_VALUE << ")";
             help.append_attributes(post_stream.str());
             post_stream.clear();
-            post_stream << "weak_classifier:\tinteger (" << DECISION_STUMP << ":DECISION_STUMP or " << RADIAL_BASIS_FUNCTION << ":RADIAL_BASIS_FUNCTION) sets the weak classifier to be used by Adaboost, (default: " << DECISION_STUMP << ")";
+            post_stream << "set_weak_classifier:\tinteger (" << DECISION_STUMP << ":DECISION_STUMP or " << RADIAL_BASIS_FUNCTION << ":RADIAL_BASIS_FUNCTION) sets the weak classifier to be used by Adaboost, (default: " << DECISION_STUMP << ")";
+            help.append_attributes(post_stream.str());
+            post_stream.clear();
+            post_stream << "add_weak_classifier:\tinteger (" << DECISION_STUMP << ":DECISION_STUMP or " << RADIAL_BASIS_FUNCTION << ":RADIAL_BASIS_FUNCTION) adds the weak classifier to the list of classifiers used by Adaboost)";
             help.append_attributes(post_stream.str());
             help.append_attributes(k_attribute_help);
         }
@@ -63,34 +66,11 @@ namespace ml
     protected:
         static void setup(t_classid c)
         {
-              
-//            /**
-//             Adds a WeakClassifier to the list of WeakClassifiers to use for boosting.
-//             
-//             If this function is called, the new WeakClassifier will be added to the list of WeakClassifiers.
-//             
-//             @return returns true if the WeakClassifier was added successfully, false otherwise
-//             */
-//            bool addWeakClassifier(const WeakClassifier &weakClassifer);
-//            
-//            /**
-//             Clears all the current WeakClassifiers.
-//             
-//             @return returns true if the WeakClassifiers was cleared successfully, false otherwise
-//             */
-//            bool clearWeakClassifiers();
-            
-            
             // Flext attribute set messages
             FLEXT_CADDATTR_SET(c, "prediction_method", set_prediction_method);
             FLEXT_CADDATTR_SET(c, "num_boosting_iterations", set_num_boosting_iterations);
-            FLEXT_CADDATTR_SET(c, "weak_classifier", set_weak_classifier);
-            
-            // Flext attribute get messages
-//            FLEXT_CADDATTR_GET(c, "type", get_type);
-            
-            // Flext method messages
-//            FLEXT_CADDMETHOD_(c, 0, "cross_validation", cross_validation);
+            FLEXT_CADDATTR_SET(c, "set_weak_classifier", set_weak_classifier);
+            FLEXT_CADDATTR_SET(c, "add_weak_classifier", add_weak_classifier)
             
             // Associate this Flext class with a certain help file prefix
             DefineHelp(c, ml_object_name.c_str());
@@ -102,9 +82,9 @@ namespace ml
         void set_prediction_method(int prediction_method);
         void set_num_boosting_iterations(int num_boosting_iterations);
         void set_weak_classifier(int weak_classifier);
+        void add_weak_classifier(int weak_classifier);
         
         // Attribute Getters
-//        void get_type(int &type) const;
         
         // Pure virtual method implementations
         GRT::Classifier &get_Classifier_instance();
@@ -112,12 +92,12 @@ namespace ml
            
     private:
         // Flext method wrappers
-//        FLEXT_CALLBACK(cross_validation);
         
         // Flext attribute wrappers
         FLEXT_CALLSET_I(set_prediction_method);
         FLEXT_CALLSET_I(set_num_boosting_iterations);
         FLEXT_CALLSET_I(set_weak_classifier);
+        FLEXT_CALLSET_I(add_weak_classifier);
 
         // Virtual method override
         virtual const std::string get_object_name(void) const { return ml_object_name; };
@@ -163,6 +143,25 @@ namespace ml
         else if (weak_classifier == RADIAL_BASIS_FUNCTION)
         {
             adaboost.setWeakClassifier(GRT::RadialBasisFunction());
+        }
+        else
+        {
+            std::stringstream error_stream;
+            error_stream << "invalid weak classifier: " << weak_classifier << " must be " << DECISION_STUMP <<
+            ":DECISION_STUMP or " << RADIAL_BASIS_FUNCTION << ": RADIAL_BASIS_FUNCTION";
+            error(error_stream.str());
+        }
+    }
+    
+    void ml_adaboost::add_weak_classifier(int weak_classifier)
+    {
+        if (weak_classifier == DECISION_STUMP)
+        {
+            adaboost.addWeakClassifier(GRT::DecisionStump());
+        }
+        else if (weak_classifier == RADIAL_BASIS_FUNCTION)
+        {
+            adaboost.addWeakClassifier(GRT::RadialBasisFunction());
         }
         else
         {
