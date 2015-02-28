16b75cc9b42c26fdec214630b9c62ad48da5d97b
Jamie Bullock <jamie@jamiebullock.com>
2015-02-28 12:44:41 +0000

    Move some implementation details into .cpp file

--- sources/ml_doc.cpp
+++ sources/ml_doc.cpp
@@ -1,9 +1,77 @@
-//
-//  ml_doc.cpp
-//  ml
-//
-//  Created by Jamie Bullock on 18/02/2015.
-//
-//
+/*
+ * ml-lib, a machine learning library for Max and Pure Data
+ * Copyright (C) 2013 Carnegie Mellon University
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "ml_doc.h"
+
+namespace ml_doc
+{
+    
+    std::string class_descriptor::print(void)
+    {
+        std::string out = name + ": " + desc + "\n";
+        
+        for(auto &attr : message_descriptors)
+        {
+            out += attr->print();
+            out += "\n";
+        }
+        return out;
+    }
+
+    
+    // ml_doc_manager implementation
+    ml_doc_manager& ml_doc_manager::shared_instance()
+    {
+        static ml_doc_manager instance;
+        instance.populate();
+        return instance;
+    }
+
+    std::string ml_doc_manager::descriptor_for_class(std::string class_name)
+    {
+        auto it = descriptors.find(class_name);
+        std::unique_ptr<class_descriptor> descriptor;
+        
+        if (it == descriptors.end())
+        {
+            return "";
+        }
+        
+        return it->second->print();
+    }
+    
+    void ml_doc_manager::populate(void)
+    {
+        std::unique_ptr<class_descriptor> ml_mlp(new class_descriptor());
+        ranged_message_descriptor<int> min_epochs;
+        
+        min_epochs.name = "min_epochs";
+        min_epochs.desc = "the minimum number of training iterations";
+        min_epochs.min = 0;
+        min_epochs.max = 100;
+        min_epochs.def = 10;
+        min_epochs.allowed_values = {1, 2, 3, 4, 5};
+        
+        ml_mlp->add_message_descriptor(min_epochs);
+        ml_mlp->name = "ml.mlp";
+        ml_mlp->desc = "blah blah blah";
+        
+        descriptors.emplace(std::pair<std::string, std::unique_ptr<class_descriptor> >("ml.mlp", std::move(ml_mlp)));
+    }
+
+}
--- sources/ml_doc.h
+++ sources/ml_doc.h
@@ -31,12 +31,14 @@
 #ifndef ml_doc_h
 #define ml_doc_h
 
-
 namespace ml_doc
 {
     class message_descriptor
     {
     public:
+        message_descriptor()
+        : is_method(false) {};
+        
         virtual std::string print(void)
         {
             std::string out = name + ": " + desc + ". ";
@@ -45,6 +47,8 @@ namespace ml_doc
 
         std::string name;
         std::string desc;
+    private:
+        const bool is_method;
     };
 
     template <typename T>
@@ -95,6 +99,8 @@ namespace ml_doc
     class class_descriptor
     {
     public:
+        class_descriptor() : parent(nullptr) {};
+        
         template <typename T>
         void add_message_descriptor(T &message_descriptor)
         {
@@ -102,77 +108,29 @@ namespace ml_doc
             message_descriptors.push_back(std::move(attr_ptr));
         }
 
-        std::string print(void)
-        {
-            std::string out = name + ": " + desc + "\n";
-
-            for(auto &attr : message_descriptors)
-            {
-                out += attr->print();
-                out += "\n";
-            }
-            return out;
-        }
-
+        std::string print(void);
+        
         std::string name;
         std::string desc;
-
+        
     private:
         std::vector<std::unique_ptr<message_descriptor> > message_descriptors;
+        class_descriptor *parent;
     };
 
     class ml_doc_manager
     {
     public:
-        static ml_doc_manager& shared_instance()
-        {
-            static ml_doc_manager instance;
-            instance.populate();
-            return instance;
-        }
-
-        std::string descriptor_for_class(std::string class_name)
-        {
-            auto it = descriptors.find(class_name);
-            std::unique_ptr<class_descriptor> descriptor;
-
-            if (it == descriptors.end())
-            {
-                return "";
-            }
-
-            return it->second->print();
-        }
-
-        bool is_empty(class_descriptor &descriptor)
-        {
-            return &descriptor == &empty_descriptor;
-        }
-
+        static ml_doc_manager& shared_instance();
+        std::string descriptor_for_class(std::string class_name);
+        
     private:
         ml_doc_manager() {};
         ml_doc_manager(ml_doc_manager const&) = delete;
         void operator=(ml_doc_manager const&) = delete;
-    
-        void populate(void)
-        {
-            std::unique_ptr<class_descriptor> ml_mlp(new class_descriptor());
-            ranged_message_descriptor<int> min_epochs;
-            
-            min_epochs.name = "min_epochs";
-            min_epochs.desc = "the minimum number of training iterations";
-            min_epochs.min = 0;
-            min_epochs.max = 100;
-            min_epochs.def = 10;
-            min_epochs.allowed_values = {1, 2, 3, 4, 5};
-            
-            ml_mlp->add_message_descriptor(min_epochs);
-            ml_mlp->name = "ml.mlp";
-            ml_mlp->desc = "blah blah blah";
-            
-            descriptors.emplace(std::pair<std::string, std::unique_ptr<class_descriptor> >("ml.mlp", std::move(ml_mlp)));
-        }
 
+        void populate(void);
+        
         std::map<std::string, std::unique_ptr<class_descriptor> > descriptors;
         class_descriptor empty_descriptor;
   };
