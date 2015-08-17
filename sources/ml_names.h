5b33b1eb96428a65809fc6bf4390daa08cb1ff45
Jamie Bullock <jamie@jamiebullock.com>
2015-08-17 15:53:04 +0100

    Add class name lookup table

--- sources/ml_names.h
+++ sources/ml_names.h
@@ -9,6 +9,8 @@
 #ifndef ml_ml_names_h
 #define ml_ml_names_h
 
+#include <set>
+
 namespace ml
 {
     const std::string k_base = "ml.base";
@@ -33,6 +35,28 @@ namespace ml
     const std::string k_linreg = "ml.linreg";
     const std::string k_logreg = "ml.logreg";
     const std::string k_mlp = "ml.mlp";
+    
+    const std::set<std::string> k_classes =
+    {
+        k_peak,
+        k_minmax,
+        k_zerox,
+        k_svm,
+        k_adaboost,
+        k_anbc,
+        k_dtw,
+        k_hmm,
+        k_softmax,
+        k_randforest,
+        k_mindist,
+        k_lda,
+        k_knn,
+        k_gmm,
+        k_dtree,
+        k_linreg,
+        k_logreg,
+        k_mlp
+    };
 }
 
 #endif
