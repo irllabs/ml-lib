819c7bc0bc00a04be143c48c85ca5e67ff8d9be4
Jamie Bullock <jamie@jamiebullock.com>
2017-09-05 09:26:27 +0100

    Setup Xcode project for iOS build

---
+++ build/macosx/ml/ml.cpp
@@ -0,0 +1,13 @@
+//
+//  ml.m
+//  ml
+//
+//  Created by Jamie Bullock on 05/09/2017.
+//
+//
+
+//#import "ml.h"
+//
+//@implementation ml
+//
+//@end
---
+++ build/macosx/ml/ml.h
@@ -0,0 +1,13 @@
+//
+//  ml.h
+//  ml
+//
+//  Created by Jamie Bullock on 05/09/2017.
+//
+//
+
+//#import <Foundation/Foundation.h>
+//
+//@interface ml : NSObject
+//
+//@end
