2e62a1f5c974c0c290c9615c0f2851f7f82d7a5b
Jamie Bullock <jamie@jamiebullock.com>
2017-09-11 12:06:13 +0100

    Add libgrt.a for iOS

---
+++ build/macosx/GRT/grt/grt.m
@@ -0,0 +1,13 @@
+//
+//  grt.m
+//  grt
+//
+//  Created by Jamie Bullock on 11/09/2017.
+//  Copyright © 2017 Jamie Bullock. All rights reserved.
+//
+
+#import "grt.h"
+
+@implementation grt
+
+@end
