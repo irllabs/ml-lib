db5c71feed565e76e309b07f1466b880471be463
Jamie Bullock <jamie@jamiebullock.com>
2017-12-14 13:40:23 +0000

    Update libpd submodule

---
+++ build/macosx/libPd-ml/libPd-ml/libPd_ml.h
@@ -0,0 +1,13 @@
+//
+//  libPd_ml.h
+//  libPd-ml
+//
+//  Created by Jamie Bullock on 11/09/2017.
+//  Copyright © 2017 Jamie Bullock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libPd_ml : NSObject
+
+@end
---
+++ build/macosx/libPd-ml/libPd-ml/libPd_ml.m
@@ -0,0 +1,13 @@
+//
+//  libPd_ml.m
+//  libPd-ml
+//
+//  Created by Jamie Bullock on 11/09/2017.
+//  Copyright © 2017 Jamie Bullock. All rights reserved.
+//
+
+#import "libPd_ml.h"
+
+@implementation libPd_ml
+
+@end
